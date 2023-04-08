26a15b9fdf26a439f81bdf6e4a6d613626e4969b
Samuel Allan <123232378+theSam1998@users.noreply.github.com>
2023-04-08 14:17:56 -0700

    Update securityautoconfig.ps1

--- securityautoconfig.ps1
+++ securityautoconfig.ps1
@@ -1,12 +1,18 @@
+#Enables Windows Firewall for all network profiles
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled True
+#Enables Microsoft Defender Antivirus realtime monitoring, providing continuous protection
 Set-MpPreference -DisableRealtimeMonitoring $false
+#Updates Windows Defender signature to provide up to date protection
 Update-MpSignature
+#sets variable interfaceIndex to represent current network adapter index number
 $interfaceIndex = (Get-NetAdapter).ifIndex
+#Configures DNS server address to use Google's public DNS
 Set-DnsClientServerAddress -InterfaceIndex $interfaceIndex -ServerAddresses "8.8.8.8"
+#Disables SMBV1, an insecure protocol
 Set-SmbServerConfiguration -EnableSMB1Protocol $false -Force
+# Enables RDP by setting value of FDenyTSConnections to 0 in Windows Registry
 Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Value 0
+# Allows incoming remote desktop connections through the firewall, for remote access by execs
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+#Sets power scheme to high performance
 powercfg.exe /setactive 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
-Set-NetFirewallRule -DisplayGroup "File And Printer Sharing" -Enabled True
-netsh advfirewall firewall add rule name="Allow incoming ping requests IPV4" dir=in action=allow protocol=icmpv4:8,any
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
