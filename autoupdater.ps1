4ea8b026eb3381330f64f6ade9fe1418f8805a3a
Samuel Allan <123232378+theSam1998@users.noreply.github.com>
2023-04-16 19:51:57 -0700

    Update autoupdater.ps1

--- autoupdater.ps1
+++ autoupdater.ps1
@@ -1,3 +1,11 @@
+#Author:Sam Allan
+#Date of last revision:04/07/2023
+#Purpose:Automatically update windows weekly
+
+#MAIN
+
 Install-Module PSWindowsUpdate -Force
 Import-Module PSWindowsUpdate
 Get-WindowsUpdate -Install -AcceptAll -IgnoreReboot
+
+#END
