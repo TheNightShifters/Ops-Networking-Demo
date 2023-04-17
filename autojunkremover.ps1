88c91b428603321d0305db534ed7477057dd1611
Samuel Allan <123232378+theSam1998@users.noreply.github.com>
2023-04-16 19:53:21 -0700

    Update autojunkremover.ps1

--- autojunkremover.ps1
+++ autojunkremover.ps1
@@ -1,2 +1,10 @@
+#Author:Sam Allan
+#Date of last revision:04/07/2023
+#Purpose:Remove temporary files older than seven days nightly for improved security and storage
+
+#MAIN
+
 $limit = (Get-Date).AddDays(-7)
 Get-ChildItem -Path "C:\Windows\Temp\*" -Recurse -Force | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt $limit } | Remove-Item -Force
+
+#END
